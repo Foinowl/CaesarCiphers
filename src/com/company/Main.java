a17c0bef4d5e4d39990fed77068a304c0edd67ec
Evgeniy <nifpopnaf@gmail.com>
2021-10-31 18:26:40 +0400

    fix decode method, remove logic with ascii-table.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
         String stringReader = InputAndOutputService.readTextFromFile("D:\\epam\\caesarChiper\\src\\com\\company\\Новый текстовый документ (2).txt");
-        System.out.println(stringReader);
 
-        System.out.println(CaesarCipherService.decode("мама мыла раму", 3));
-        System.out.println(CaesarCipherService.encode("пгпг пюог угпц", 3));
+//        System.out.println(CaesarCipherService.decode("мама, мыла: раму?", 3));
+//        System.out.println(CaesarCipherService.encode("пгпг пюог угпц", 3));
         System.out.println(CaesarCipherService.bruteForceEncode(stringReader));
     }
 }
--- src/com/company/service/CaesarCipherService.java
+++ src/com/company/service/CaesarCipherService.java
@@ -1,27 +1,31 @@
 package com.company.service;
 
+import java.util.Locale;
 import java.util.regex.Pattern;
 
 public class CaesarCipherService {
-    private static final char FIRST_LETTER = 'а';
-    private static final char LAST_LETTER = 'я';
-    private static final int ALPHABET_SIZE = LAST_LETTER - FIRST_LETTER + 1;
-
+    private static final String upperLetters = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    private static final String lowerLetters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    private static final char[] upperListLetters = upperLetters.toCharArray();
+    private static final char[] lowerListLetters = lowerLetters.toCharArray();
+    private static final int ALPHABET_SIZE = upperLetters.length();
 
     public static String decode(String text, int shift) {
         StringBuilder stringBuilder = new StringBuilder();
 
         for (char character : text.toCharArray()) {
-            if (!(Pattern.matches("[.!?\\-\\s]", String.valueOf(character)))) {
-                int originalAlphabetPosition = character - FIRST_LETTER;
-                int newAlphabetPosition = (originalAlphabetPosition + shift) % ALPHABET_SIZE;
-                stringBuilder.append((char) (FIRST_LETTER + newAlphabetPosition));
+            if (!(Pattern.matches("[&*^$#@;:.,!?\\-\\s]", String.valueOf(character)))) {
+                int alphabetPosition = upperLetters.indexOf(String.valueOf(character).toUpperCase(Locale.ROOT));
+                int newAlphabetPosition = (alphabetPosition + shift) % ALPHABET_SIZE;
+                stringBuilder.append(Character.isUpperCase(character)
+                        ? upperListLetters[newAlphabetPosition]
+                        : lowerListLetters[newAlphabetPosition]);
             } else {
                 stringBuilder.append(character);
             }
         }
 
-        return  stringBuilder.toString();
+        return stringBuilder.toString();
     }
 
     public static String encode(String text, int shift) {
@@ -35,14 +39,16 @@ public class CaesarCipherService {
         InputAndOutputService.writeMessage("\nЕсли сообщение не оказалась расшифрованным, нажмите '0'\n");
 
         int keyShift = 1;
-        while (keyShift <= ALPHABET_SIZE) {
-            String encryptText = encode(text, keyShift++);
+        String encryptText = null;
+        while (keyShift <= 33) {
+            encryptText = encode(text, keyShift);
             InputAndOutputService.writeMessage(encryptText);
             int messageFromUser = InputAndOutputService.readInt();
             if (messageFromUser == 1) {
-                return encryptText;
+                break;
             }
+            keyShift++;
         }
-        return null;
+        return encryptText;
     }
 }
